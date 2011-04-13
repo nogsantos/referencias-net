2a1f594a567f371992a8e6bfe0c88559d82b5844
nogsantos@gmail.com <nogsantos@gmail.com@3484d7cc-72db-da22-8584-234e2fb92d3d>
2011-04-13 23:03:12 +0000

    atualização

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
-header("Location:home.php");
+header("Location:app/viewer/index.php");
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
