afc166484f220ebd33b96f726b3d47a0ac5b0570
nogsantos <nogsantos@3484d7cc-72db-da22-8584-234e2fb92d3d>
2011-04-14 17:39:35 +0000

    implementação do index

--- app/viewer/cadReferencia.php
+++ app/viewer/cadReferencia.php
@@ -15,22 +15,41 @@ $oTipos        = new Tipo();
 $resTipos      = new Resultset();
 $resTipos      = $oTipos->consultaTodosTipo();
 
+$numgReferencia = $_GET["numgReferencia"]!="" ? $_GET["numgReferencia"] : "" ;
+
 if($numgReferencia!=""){
     $resReferencia = $oReferencia->consultarReferencia($numgReferencia);
 }
 
+switch ($_GET["msg"]) {
+    case 1:$msg  = "Cadastro realizado.";break;
+    case 2:$msg  = "Edição realizada.";break;
+    case 3:$msg  = "Exclusão realizada.";break;
+    default:$msg = "";break;
+}
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+        <meta http-equiv="Content-Type" content="text/html;">
         <title>Fabricio Nogueira  - Cadastro das referências</title>
         <link rel="stylesheet" type="text/css" media="screen" href="../../public/css/estilo.css" />
         <script type="text/javascript" src="../../public/js/jquery-1.5.2.min.js"></script>
         <script type="text/javascript">
             $(document).ready(function(){
+                $("#btNovo").click(function(){
+                    document.location = "cadReferencia.php";
+                });
                 $("#btCadastrar").click(function(){
                     $("#sFuncao").val("cadastrar");
+                    form.submit();
+                });
+                $("#btEditar").click(function(){
+                    $("#sFuncao").val("editar");
+                    form.submit();
+                });
+                $("#btExcluir").click(function(){
+                    $("#sFuncao").val("excluir");
                     form.submit();
                 });
             });
@@ -39,8 +58,12 @@ if($numgReferencia!=""){
     <body>
         <?include("../../public/includes/inc.menu.php");?>
         <h1>Cadastro de referências</h1>
+        <?if($msg!=""){?>
+        <div style="width: 100%;text-align: left;color: blue"><?=$msg?></div>
+        <?}?>
         <form name="form" id="form" method="post" action="../controller/Referencia.controller.php">
             <input type="hidden" name="sFuncao" id="sFuncao" value="" />
+            <input type="hidden" name="numgReferencia" id="numgReferencia" value="<?=$numgReferencia?>" />
             <div id="divMain">
                 <table border="0" cellpadding="0" cellspacing="0" width="100%" align="center">
                     <tr>
@@ -54,18 +77,24 @@ if($numgReferencia!=""){
                     <tr>
                         <td>
                             Url<br />
-                            <input type="text" name="descUrl" id="descUrl" value="" size="102" />
+                            <input type="text" name="descUrl" id="descUrl" value="<?=$resReferencia->getValores(0, "desc_url")?>" size="102"  />
                         </td>
                     </tr>
                     <tr>
                         <td>
                             Observa&ccedil;&atilde;o<br />
-                            <textarea name="descObs" id="descObs" cols="100" rows="20"></textarea>
+                            <textarea name="descObs" id="descObs" cols="100" rows="20"><?=$resReferencia->getValores(0, "desc_obs")?></textarea>
                         </td>
                     </tr>
                     <tr>
                         <td valign="middle" align="left">
-                            <input type="button" id="btCadastrar" value="Cadastrar" />
+                            <?if($numgReferencia==""){?>
+                                <input type="button" id="btCadastrar" class="botao" value="Cadastrar" />
+                            <?}else{?>
+                                <input type="button" id="btNovo" class="botao" value="Nova Referencia" />
+                                <input type="button" id="btEditar" class="botao" value="Editar" />
+                                <input type="button" id="btExcluir" class="botao" value="Excluir" />
+                            <?}?>
                         </td>
                     </tr>
                 </table>
--- app/viewer/index.php
+++ app/viewer/index.php
@@ -1,16 +1,133 @@
 <?php
-/* 
+/*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+require_once("../../public/Funcoes.php");
+require_once("../dataBase/Resultset.php");
+require_once("../dataBase/Oad.php");
+require_once("../model/Referencia.model.php");
 
+$resReferencia = Referencia::consultarTodasReferencia();
 ?>
 <html>
     <head>
         <title>Fabricio Nogueira</title>
     </head>
     <link rel="stylesheet" type="text/css" media="screen" href="../../public/css/estilo.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="../../public/css/custom.css" />
+    <script type="text/javascript" src="../../public/js/jquery-1.5.2.min.js"></script>
+    <script type="text/javascript" src="../../public/js/jquery.tablesorter.min.js"></script>
+    <script type="text/javascript" src="../../public/js/jquery.tablesorter.pager.js"></script>
     <body>
-        <?include("../../public/includes/inc.menu.php");?>
+        <? include("../../public/includes/inc.menu.php"); ?>
+        <form method="post" action="exemplo.html" id="frm-filtro">
+            <p>
+                <label for="pesquisar">Pesquisar</label>
+                <input type="text" id="pesquisar" name="pesquisar" size="30" />
+            </p>
+        </form>
+        <table cellpadding="0" cellspacing="0" width="100%">
+            <thead>
+                <tr>
+                    <th><input type="checkbox" value="1" id="marcar-todos" name="marcar-todos" /></th>
+                    <th align="center" >ID</th>
+                    <th align="center" >Tipo</th>
+                    <th align="left">Url</th>
+                    <th align="left">Descri&ccedil;&atilde;o</th>
+                    <th align="center">Dt. Cadastro</th>
+                    <th align="center">A&ccedil;&otilde;es</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?for($i=0;$i<$resReferencia->getCount();$i++){?>
+                <tr>
+                    <td align="center"><input type="checkbox" value="1" name="marcar[]" /></td>
+                    <td align="center"><?=$resReferencia->getValores($i, "numg_referencia")?></td>
+                    <td align="center"><?=$resReferencia->getValores($i, "numg_tipo")?></td>
+                    <td align="left"><a href="<?=$resReferencia->getValores($i, "desc_url")?>" target="_blank"><?=$resReferencia->getValores($i, "desc_url")?></a></td>
+                    <td><?=$resReferencia->getValores($i, "desc_obs")?></td>
+                    <td align="center"><?=Funcoes::FormataData($resReferencia->getValores($i, "data_cadastro"))?></td>
+                    <td align="center">
+                        <a href="#"><img src="../../public/imagens/edit.png" width="16" height="16" alt="Editar" title="Editar" /></a>
+                        <a href="#"><img src="../../public/imagens/delete.png" width="16" height="16" alt="Deletar" title="Deletar" /></a>
+                    </td>
+                </tr>
+                <?}?>
+            </tbody>
+        </table>
+        <div id="pager" class="pager" style="position:relative;float: right">
+            <form>
+                <span>
+                        Exibir <select class="pagesize">
+                        <option selected="selected"  value="10">10</option>
+                        <option value="20">20</option>
+                        <option value="30">30</option>
+                        <option  value="40">40</option>
+                    </select> registros
+                </span>
+                <img src="../../public/imagens/first.png" class="first"/>
+                <img src="../../public/imagens/prev.png" class="prev"/>
+                <input type="text" class="pagedisplay"/>
+                <img src="../../public/imagens/next.png" class="next"/>
+                <img src="../../public/imagens/last.png" class="last"/>
+            </form>
+        </div>
+        <script type="text/javascript">
+            $(function(){
+
+                $('table > tbody > tr:odd').addClass('odd');
+
+                $('table > tbody > tr').hover(function(){
+                    $(this).toggleClass('hover');
+                });
+
+                $('#marcar-todos').click(function(){
+                    $('table > tbody > tr > td > :checkbox')
+                    .attr('checked', $(this).is(':checked'))
+                    .trigger('change');
+                });
+
+                $('table > tbody > tr > td > :checkbox').bind('click change', function(){
+                    var tr = $(this).parent().parent();
+                    if($(this).is(':checked')) $(tr).addClass('selected');
+                    else $(tr).removeClass('selected');
+                });
+
+                $('form').submit(function(e){ e.preventDefault(); });
+
+                $('#pesquisar').keydown(function(){
+                    var encontrou = false;
+                    var termo = $(this).val().toLowerCase();
+                    $('table > tbody > tr').each(function(){
+                        $(this).find('td').each(function(){
+                            if($(this).text().toLowerCase().indexOf(termo) > -1) encontrou = true;
+                        });
+                        if(!encontrou) $(this).hide();
+                        else $(this).show();
+                        encontrou = false;
+                    });
+                });
+
+                $("table")
+                .tablesorter({
+                    dateFormat: 'uk',
+                    headers: {
+                        0: {
+                            sorter: false
+                        },
+                        5: {
+                            sorter: false
+                        }
+                    }
+                })
+                .tablesorterPager({container: $("#pager")})
+                .bind('sortEnd', function(){
+                    $('table > tbody > tr').removeClass('odd');
+                    $('table > tbody > tr:odd').addClass('odd');
+                });
+
+            });
+        </script>
     </body>
 </html>
